fdde7e24413c06a09e4e1ce508f4408d5aab1b98
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-08-21 13:41:58 +0200

    products

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,1 +1,21 @@
-// comments
+const express = require("express");
+const bodyParser = require("body-parser");
+const {verifyAToken} = require('../middleware/Authenticate')
+const routes = express.Router();
+
+const { users, products } = require("../model");
+
+routes.get("/users", (req, res) => {
+    users.fetchUsers(req, res);
+  });
+  
+  routes.get("/user/:id", (req, res) => {
+    users.fetchUser(req, res);
+  });
+
+
+module.exports = {
+    express,
+    routes,
+    verifyAToken
+}
